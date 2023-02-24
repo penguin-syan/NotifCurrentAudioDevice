cff7e7196873cdd610c77cc12cc99d9def321f22
penguin-syan <member@penguin-syan.tokyo>
2023-02-24 18:21:49 +0900

    first commit

---
+++ ToastCurrentAudioDevice/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Toolkit.Uwp.Notifications;
+using NAudio.CoreAudioApi;
+
+// アプリ通知に表示するため、オーディオ出力先を取得
+var enumerator = new MMDeviceEnumerator();
+var result = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+
+// 連続切り替え時にアプリ通知を更新するために通知履歴を削除
+ToastNotificationManagerCompat.History.Clear();
+
+// 変更後のオーディオ出力先をアプリ通知
+new ToastContentBuilder()
+    .AddArgument("action", "viewConversation")
+    .AddArgument("conversationId", 9813)
+    .AddText("オーディオ出力先を変更")
+    .AddText(String.Format("{0}", result))
+    .Show();
